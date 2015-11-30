7917d7b6ce94161794ea44d29dec43010c2ccd7e
Xinyu Miao <hermit412@gmail.com>
2015-11-30 08:29:44 +0000

    isbnSearch modification

--- test_database_with_cache.rb
+++ test_database_with_cache.rb
@@ -41,16 +41,17 @@ describe DatabaseWithCache do
             expect(@memcached_mock).to receive(:get).with('v_1111').and_return 1
             expect(@memcached_mock).to receive(:get).with('1111_1'). and_return @book1111.to_cache
             result = @target.isbnSearch('1111')
-            expect(result).to eq @book1111 end
+            expect(result).to eq @book1111 
+          end
         context "and up to date with the remote cache" do
-              it "should use the local cache version"
-              expect(@database_mock).to_not receive(:isbnSearch) 
-              expect(@memcached_mock).to receive(:get).with('v_1111').and_return 1 
-              expect(@memcached_mock).to_not receive(:get).with('1111_1')
-              result = @target.isbnSearch('1111')
-              expect(result).to eq @book1111 
-              end
-            end
+          it "should use the local cache version" do
+            expect(@database_mock).to_not receive(:isbnSearch) 
+            expect(@memcached_mock).to receive(:get).with('v_1111').and_return 1 
+            expect(@memcached_mock).to_not receive(:get).with('1111_1')
+            result = @target.isbnSearch('1111')
+            expect(result).to eq @book1111 
+          end
+        end
         context "and outdate from the remote cache" do
           it "should use the remote cache version and update the version of local cache" do
             expect(@database_mock).to_not receive(:isbnSearch) 
@@ -58,24 +59,18 @@ describe DatabaseWithCache do
             expect(@memcached_mock).to receive(:get).with('1111_2').and_return @book1111.to_cache
             result = @target.isbnSearch('1111')
             expect(result).to eq @book1111 
-            end
-          end 
-        end
+          end
+        end 
       end
-        context"Given the book ISBN is not valid" do
-          it "should return nil" do
-            expect(@database_mock).to receive(:isbnSearch).with('1234').and_return nil 
-            expect(@memcached_mock).to receive(:get).with('v_1234').and_return nil 
-            result = @target.isbnSearch('1234')
-            expect(result).to be nil
-          end 
-        end
-      end
-
-
-          end   
-        end
+      context"Given the book ISBN is not valid" do
+        it "should return nil" do
+          expect(@database_mock).to receive(:isbnSearch).with('1234').and_return nil 
+          expect(@memcached_mock).to receive(:get).with('v_1234').and_return nil 
+          result = @target.isbnSearch('1234')
+          expect(result).to be nil
+        end 
       end
     end
-    
+  end     
 end
+  
